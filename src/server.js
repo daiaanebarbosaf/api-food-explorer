03f7b7f2fe08cfb102d536437e5d4b5748f843dd
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-26 09:35:24 -0300

    Como desestruturar dados para não repetir código.

--- src/server.js
+++ src/server.js
@@ -2,5 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params;
+
+        response.send(`
+        Mensagem ID: ${id}.
+        Para o usuário: ${user}.
+    `);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`O servidor está rodando na Porta ${PORT}`));
