1cacd458eeb31c9a784087e96b6418515f84286c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-29 18:18:01 -0300

    Removendo a configuração do Middleware
    Esse foi apenas um teste, em breve farei da forma como ele será utilizado.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,8 +2,10 @@ class UsersController {
     create(request, response){
         const { name, email, password } = request.body;
 
-        response.status(201).json({ name });
+        response.status(201).json({ name, email, password });
     }
-};
+}
+
+
 
 module.exports = UsersController;
