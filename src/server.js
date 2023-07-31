fe38ffb7a32e3e37fff669cfc96bda471dde7e9e
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-07-31 09:13:20 -0300

    Criando configuração para conexão com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 require("express-async-errors");
 
+const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
 
 const express = require("express");
 
 const routes = require("./routes");
 
+database();
+
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
 
 app.use((error, request, response, next) =>{
     if( error instanceof AppError ){
