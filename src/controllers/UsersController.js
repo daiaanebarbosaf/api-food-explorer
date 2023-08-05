4399499d26fe5821ae0251015e316e58bcba856c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-05 09:46:07 -0300

    Tratamento de erro para verificar se o e-mail cadastrado já existe

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,8 +3,17 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
-    create(request, response){
+    async create(request, response){
         const { name, email, password } = request.body;
+
+        const database = await sqliteConnection();
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(checkUserExists){
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
+        return response.status(201).json();
     }
 }
 
