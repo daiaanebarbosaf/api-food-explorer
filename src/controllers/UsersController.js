5a1c9ed28bedbff9d3b1daad55f590b4b3ae8520
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-05 09:35:57 -0300

    Conectando com o BD

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,10 @@
 const AppError = require("../utils/AppError");
 
+const sqliteConnection = require('../database/sqlite');
+
 class UsersController {
     create(request, response){
         const { name, email, password } = request.body;
-
-        if(!name){
-            throw new AppError("Nome é obrigatório!");
-        }
-
-        response.status(201).json({ name, email, password });
     }
 }
 
