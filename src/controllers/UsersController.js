90464c677396b0373e4f2a5c1cdf0969b8934582
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-08-05 14:42:47 -0300

    Criando a função de Update de Nome e E-mail

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -26,7 +26,7 @@ class UsersController {
 
     async update(request, response){
         const { name, email } = request.body;
-        const { id } = request.body;
+        const { id } = request.params;
 
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
@@ -37,9 +37,23 @@ class UsersController {
 
         const userWithUpdateUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-        if(userWithUpdateUpdateEmail && userWithUpdateUpdateEmail.id !== id){
+        if(userWithUpdateUpdateEmail && userWithUpdateUpdateEmail.id !== user.id){
             throw new AppError("Error e-mail já está em uso.");
         }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
     }
 }
 
