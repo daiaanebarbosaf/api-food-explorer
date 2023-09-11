8225f7382869c9db68bfd7840b2d775b4e340c64
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-11 09:45:21 -0300

    Update createUsers.js

--- src/database/sqlite/migrations/createUsers.js
+++ src/database/sqlite/migrations/createUsers.js
@@ -1,13 +1,16 @@
-const createUsers = `
-	CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name VARCHAR,
-		email VARCHAR,
-		password VARCHAR,
-		is_admin BIT,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	)
-`;
+exports.up = knex => knex.schema.createTable("users", table => {
+	table.increments("id");
+	table.text("name").notNullable();
+	table.text("email").notNullable();
+	table.text("password").notNullable();
 
-module.exports = createUsers;
+	table
+		.enum("role", ["admin","customer"], { useNative:true, enumName: "roles"})
+		.notNullable().default("customer")
+
+	table.timestamp("created_at").default(knex.fn.now());
+	table.timestamp("updated_at").default(knex.fn.now());
+
+});
+
+exports.down = knex => knex.schema.dropTable("users");
