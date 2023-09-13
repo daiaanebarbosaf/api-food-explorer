9781b395f1a1dad811989c5de514dd257f3def7a
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-13 08:32:18 -0300

    Aplicando middlewares para todas as rotas

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 
 const DishesController = require("../controllers/DishesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const dishesRoutes = Router();
 
 const dishesController = new DishesController();
+
+dishesRoutes.use(ensureAuthenticated);
 
 dishesRoutes.get("/", dishesController.index);
 dishesRoutes.post("/:user_id", dishesController.create);
