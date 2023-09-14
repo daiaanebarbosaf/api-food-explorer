9d52ccebd707c00b5db4ed67c921694bdb349727
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-14 10:02:27 -0300

    criando rota de uploads de imagem

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const DishesController = require("../controllers/DishesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
 const dishesController = new DishesController();
 
@@ -13,5 +16,9 @@ dishesRoutes.get("/", dishesController.index);
 dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.patch("/dish", ensureAuthenticated, upload.single("dish"), (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+})
 
 module.exports = dishesRoutes;
