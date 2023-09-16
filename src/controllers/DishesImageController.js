fa4d48b1d672bad9af70042410b9f757a131ef01
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-09-16 16:28:36 -0300

    Criando a controller para adicionar as imagens do prato

---
+++ src/controllers/DishesImageController.js
@@ -0,0 +1,33 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const DiskStorange = require("../providers/DiskStorange");
+
+class DishesImageController {
+    async update(request, response){
+        const dish_id = request.user.id;
+
+        const imgdishFilename = request.file.filename;
+
+        const diskStorange = new DiskStorange();
+
+        const dish = await knex("dishes")
+        .where({ id: dish_id }).first();
+
+        if(!dish){
+            throw new AppError("Somente usuário compermissão de Administrador pode mudar a imagem", 401);
+        }
+
+        if(dish.imgdish){
+            await diskStorange.deleteFile(dish.imgdish);
+        }
+
+        const filename = await diskStorange.saveFile(imgdishFilename);
+        dish.imgdish = filename;
+
+        await knex("dishes").update(dish).where({ id: dish_id });
+
+        return response.json(dish);
+    }
+}
+
+module.exports = DishesImageController;
