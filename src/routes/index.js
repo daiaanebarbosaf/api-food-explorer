9dbe8ee872c0bac3f4b47e682729feae9d5cd88c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-04 13:01:49 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
 const tagsRouter = require("./tags.routes");
 const sessionsRouter = require("./sessions.routes");
+const adminRouter = require("./admin.routes");
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use("/users", usersRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/tags", tagsRouter);
 routes.use("/sessions", sessionsRouter);
+routes.use("/admin", adminRouter);
 
 module.exports = routes;
