c77c6b32742570745ea060c169541ea8c8863ea6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-04 13:01:10 -0300

    rota de admin

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+
+const AdminController = require("../controllers/AdminController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
+
+const adminRoutes = Router();
+const adminController = new AdminController();
+
+adminRoutes.use(ensureAuthenticated);
+adminRoutes.use(verifyUserAuthorization("admin"));
+adminRoutes.get("/", adminController.index);
+
+module.exports = adminRoutes;
