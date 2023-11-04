179357ba7c192f2b47eba699a2bd5ae7cae7aaa8
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-04 11:37:37 -0300

    Adaptação para nova migrate de criação de usuário funcionar.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,26 +1,23 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-const sqliteConnection = require('../database/sqlite');
+const sqliteConnection = require('../database/knex');
+const knex = require("../database/knex");
 const { application } = require("express");
 
 class UsersController {
     async create(request, response){
         const { name, email, password } = request.body;
 
-        const database = await sqliteConnection();
-        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+ 
+        const checkUserExists = await knex("users").where({ email });
 
-        if(checkUserExists){
+        if(checkUserExists.length > 0){
             throw new AppError("Este e-mail já está em uso.")
         }
 
-        const hashedPassoword = await hash(password, 8);
+        const hashedPassword = await hash(password, 8);
 
-        await database.run(
-            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, hashedPassoword]
-        
-        );
+        await knex("users").insert({ name, email, password: hashedPassword });
 
         return response.status(201).json();
     }
