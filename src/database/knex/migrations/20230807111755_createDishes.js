f031009e12bcdd763d300565672a32394d835a2d
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-11-09 10:38:35 -0300

    Update 20230807111755_createDishes.js

--- src/database/knex/migrations/20230807111755_createDishes.js
+++ src/database/knex/migrations/20230807111755_createDishes.js
@@ -1,10 +1,10 @@
 
 exports.up = knex => knex.schema.createTable("dishes", table => {
     table.increments("id");
-    table.text("title");
-    table.text("description");
-    table.text("categoty");
-    table.real("price");
+    table.text("title").notNullable();
+    table.text("description").notNullable();
+    table.text("categoty").notNullable();
+    table.real("price", 10, 2).notNullable();
     table.varchar("imgdish");
     table.integer("user_id").references("id").inTable("users");
 
